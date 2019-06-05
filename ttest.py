7882b964a78f14b44f518c623b3d58089d67c429
sunxiaopan <51370164+sunxiaopan@users.noreply.github.com>
2019-06-05 14:14:23 +0800

    add new info
    genggai

---
+++ ttest.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import requests
+import base64
+import json
+import random
+import string
+from Crypto.Cipher import AES
+import urllib3
+import time
+import hashlib
+from locust  import HttpLocust,TaskSet,task
+class student():
+    def __init__(self):
+        pass
+    def tets(self):
+        pass
